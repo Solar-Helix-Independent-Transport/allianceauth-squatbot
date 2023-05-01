f0b54e9db8acdb53953c6058c2dd05e5a5e23a07
Aaron Kable <aaronkable@gmail.com>
2023-05-01 19:02:24 +0800

    RepBot

--- squatbot/cog.py
+++ squatbot/cog.py
@@ -27,19 +27,21 @@ class Squats(commands.Cog):
     """
     squat_commands = SlashCommandGroup("squatbot", "AuthBot Demands Squats!", guild_ids=[
                                        int(settings.DISCORD_GUILD_ID)])
+    rep_commands = SlashCommandGroup("repbot", "AuthBot Demands REPS!", guild_ids=[
+                                       int(settings.DISCORD_GUILD_ID)])
 
-    @squat_commands.command(name='status', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    @rep_commands.command(name='status', guild_ids=[int(settings.DISCORD_GUILD_ID)])
     async def slash_stat(
         self,
         ctx,
     ):
         """
-            Show the current Squat Deficit/Surplus
+            Show the current Rep Deficit/Surplus
         """
         c = cache.get(constants.LOSS_KEY, {})
         month_key = timezone.now().strftime(constants.TZ_STRING)
         month = c.get(month_key, {})
-        losses = month.get(constants.JSON_LOS_KEY, 0)
+        losses = month.get(constants.JSON_LOS_KEY, 0) * 5
         current = cache.get(constants.SQUAT_KEY, {month_key: {}})
         total = 0
         for x in current[month_key].values():
@@ -51,22 +53,22 @@ class Squats(commands.Cog):
         extra_message = cache.get(constants.MESSAGE_KEY, "")
 
         # tasks.sqb_sync_losses.delay()
-        e = Embed(title="SquatBot",
-                  description=f"`{self.ALLIANCE.alliance_name}` has lost {losses} ships, Authbot Demands Squats for each Loss!\n{extra_message}\nUse `/squatbot claim` to get swole! :muscle: \nNO CHEATING AuthBot will know!! :eyes:\n\n**Top 10 leaderboard:**\n```\n{message}\n```")
+        e = Embed(title="RepBot!",
+                  description=f"`{self.ALLIANCE.alliance_name}` has lost {losses} ships this month, Authbot Demands 5 Reps for each Loss!\n{extra_message}\nUse `/squatbot claim` to get swole! :muscle: \nNO CHEATING AuthBot will know!! :eyes:\n\n**Top 10 leaderboard:**\n```\n{message}\n```")
 
-        e.add_field(name="Required Squats", value=f"{losses}")
+        e.add_field(name="Required Reps", value=f"{losses}")
         if losses - total > 0:
-            e.add_field(name="Squat Deficit",
+            e.add_field(name="Rep Deficit",
                         value=f"{losses - total}", inline=False)
         else:
-            e.add_field(name="Squat Surplus",
+            e.add_field(name="Rep Surplus",
                         value=f"{abs(losses - total)}", inline=False)
 
         return await ctx.respond(embed=e)
 
 
-    @squat_commands.command(name='claim', guild_ids=[int(settings.DISCORD_GUILD_ID)])
-    @option("count", int, min_value=5, max_value=50, description="Number of squats to claim!",)
+    @rep_commands.command(name='claim', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    @option("count", int, min_value=10, max_value=100, description="Number of reps to claim!",)
     async def slash_claim(
         self,
         ctx,
@@ -86,17 +88,17 @@ class Squats(commands.Cog):
         current[month_key][main_character] = user + count
         setted = cache.set(constants.SQUAT_KEY, current, 60*60*24*90)
         
-        losses = month.get(constants.JSON_LOS_KEY, 0)
+        losses = month.get(constants.JSON_LOS_KEY, 0) * 5
         current = cache.get(constants.SQUAT_KEY, {month_key: {}})
         total = 0
-        for x in current[month_key].values():
+        for x in current.get(month_key,{}).values():
             total += x
         if total > losses:
-            return await ctx.respond(f"{main_character} claimed {count} squats! We are now ahead of the losses! HELL YEAH! :muscle:\nKeep it up!\nhttps://media3.giphy.com/media/Ub4kWebdWWJP2/giphy.gif")
-        return await ctx.respond(f"{main_character} claimed {count} squats! Hell YEAH! :muscle:")
+            return await ctx.respond(f"{main_character} claimed {count} reps! We are now ahead of the losses! HELL YEAH! :muscle:\nKeep it up!")
+        return await ctx.respond(f"{main_character} claimed {count} reps! Hell YEAH! :muscle:")
 
 
-    @squat_commands.command(name='message', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    @rep_commands.command(name='message', guild_ids=[int(settings.DISCORD_GUILD_ID)])
     @option("message", description="What extra message do you want?",)
     async def slash_claim(
         self,
