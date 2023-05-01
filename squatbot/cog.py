57debe9dc7ec4f198bf2d823bcdbc8a4933ba374
Aaron Kable <aaronkable@gmail.com>
2023-05-01 19:34:41 +0800

    wrong stats

--- squatbot/cog.py
+++ squatbot/cog.py
@@ -76,7 +76,7 @@ class Squats(commands.Cog):
 
         # tasks.sqb_sync_losses.delay()
         e = Embed(title="RepBot!",
-                  description=f"`{self.ALLIANCE.alliance_name}` has lost {losses} ships this month, Authbot Demands 5 Reps for each Loss!\n{extra_message}\nUse `/repbot claim` to get swole! :muscle: \nNO CHEATING AuthBot will know!! :eyes:\n\n**Top 10 leaderboard:**\n```\n{message}\n```")
+                  description=f"`{self.ALLIANCE.alliance_name}` has lost {losses/5} ships this month, Authbot Demands 5 Reps for each Loss!\n{extra_message}\nUse `/repbot claim` to get swole! :muscle: \nNO CHEATING AuthBot will know!! :eyes:\n\n**Top 10 leaderboard:**\n```\n{message}\n```")
 
         e.add_field(name="Required Reps", value=f"{losses}")
         if losses - total > 0:
